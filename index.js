e9c4a3f1ac598717b272d3c75b115faa5029e11d
Damien <connectingreflections@gmail.com>
2018-08-14 18:01:24 -0600

    CSS, alerts moved, new objects added.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-var humidity;
-var weatherIcon;
-var pressure;
-var uvIndex;
-var temperature;
-var temperatureIcon;
-var windBearing;
-var windSpeed;
-var weatherSummary;
-var alerts;
-var summary;
-var currentTime;
+let humidity;
+let weatherIcon;
+let pressure;
+let uvIndex;
+let temperature;
+let temperatureIcon;
+let windBearing;
+let windSpeed;
+let weatherSummary;
+let alertsTitle;
+let summary;
+let forecastValidTime;
+let object;
+let alertUrl;
+
 
 window.onload = function() {
 	humidity = document.getElementById("current-humidity");
@@ -20,21 +23,34 @@ window.onload = function() {
 	temperatureIcon = document.getElementById("temperature-icon");
 	windBearing = document.getElementById("current-wind-bearing");
 	windSpeed = document.getElementById("current-wind-speed");
-	weatherSummary = document.getElementById("weather-summary");
-	if (alerts) {
-		alerts = document.getElementById("alerts");
-	} else {
-		alerts = "No Alerts as of " + timeToStandard(object.hourly.data.time);
-	}
-	summary = document.getElementById("summary");
-}
+	windGust = document.getElementById("current-wind-gust");
+	cloudCover = document.getElementById("current-cloud-cover");
+	weatherSummary = document.getElementById("weatherSummary");
+	alertsTitle = document.getElementById("alerts");
+};
+
+/*function timeToStandard(time) {
+	console.log(time);
+	let forecastTime = new Date(time * 1000);
+	let forecastHours = forecastTime.getHours();
+	let forecastMinutes = forecastTime.getMinutes();
+	return (forecastHours + ":" + forecastMinutes);
+}*/
 
 function timeToStandard(time) {
-	let forcastTime = (time*1000);
-	forcastHours = forcastTime.getHours();
-	forcastMinutes = forcastTime.getMinutes();
-	return (forcastHours + ":" + forcastMinutes);
+// Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+// Hours part from the timestamp
+	const hours = forecastValidTime.getHours();
+// Minutes part from the timestamp
+	const minutes = "0" + forecastValidTime.getMinutes();
+// Seconds part from the timestamp
+	const seconds = "0" + forecastValidTime.getSeconds();
+
+// Will display time in 10:30:23 format
+	return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 }
+
 function farenheitToCelsius(k) {
 	return Math.round((k - 32) * 0.5556 );
 }
@@ -47,7 +63,10 @@ function degreesToDirection(degrees) {
 	var range = 360/16;
 	var low = 360 - range/2;
 	var high = (low + range) % 360;
-	var angles = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+	var angles = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+	if (!degrees) {
+		return "Calm"
+	}
 	for (i in angles) {
 
 		if(degrees>= low && degrees < high)
@@ -76,7 +95,7 @@ let weatherImages = {
 	"hail": "http://icons.iconarchive.com/icons/icons8/ios7/256/Weather-Hail-icon.png",
 	"thunderstorm": "http://findicons.com/files/icons/2613/android_weather_extended/480/thunderstorms.png",
 	"tornado": "http://hddfhm.com/images/clipart-of-a-tornado-11.png"
-}
+};
 
 //var getWeather = function() {
 // 	if(navigator.geolocation){
@@ -94,34 +113,61 @@ let weatherImages = {
 let getWeather = function () {
 	let lat = 51.0253;
 	let long = -114.0499;
+//	let lat = 48.6264;
+//	let long = -77.8782;
 	showWeather(lat, long)
 };
 
 function showWeather(lat, long) {
-	var url = `https://api.darksky.net/forecast/459d95a8fbfada306d82991efda5b383/${lat},${long}` + `?format=jsonp&callback=displayWeather`;
-	var script = document.createElement("script");
+	let url = `https://api.darksky.net/forecast/459d95a8fbfada306d82991efda5b383/${lat},${long}` + `?format=jsonp&callback=displayWeather`;
+	let script = document.createElement("script");
 	script.type = "text/javascript";
 	script.src = url;
 	document.getElementsByTagName("head")[0].appendChild(script);
 	displayWeather(object)
 }
 
-let object;
 
 function displayWeather(object) {
 	humidity.innerHTML = "Humidity: " + humidityPercentage(object.currently.humidity) + "%";
 	weatherIcon.src = weatherImages[object.currently.icon];
 	pressure.innerHTML = "Pressure: " + object.currently.pressure + " mb";
 	uvIndex.innerHTML = "uvIndex: " + object.currently.uvIndex;
-	temperature.innerHTML = farenheitToCelsius(object.currently.temperature) + " C" + " / " + object.currently.temperature + " F";
+	temperature.innerHTML = farenheitToCelsius(object.currently.temperature) + " C" + " / " + Math.round(object.currently.temperature) + " F";
 	temperatureIcon.src = "https://cdn4.iconfinder.com/data/icons/medicons-2/512/thermometer-512.png";
 	windBearing.innerHTML = "Wind Direction: " + degreesToDirection(object.currently.windBearing);
 	windSpeed.innerHTML = "Wind Speed: " + knotsToKilometres(object.currently.windSpeed) + " km/h";
-	weatherSummary.innerHTML = "Current Location: " + object.timezone + " <br/> <br/> Weather Summary: " + object.currently.summary;
+	windGust.innerHTML = "Wind Gusts: " + knotsToKilometres(object.currently.windGust) + " km/h";
+	cloudCover.innerHTML = "Cloud Cover: " + humidityPercentage(object.currently.cloudCover) + "%";
+	weatherSummary.innerHTML = "<span class='summaryTitles'> Current Location:  </span>" + object.timezone + "<br/> <br/><span class='summaryTitles'> Weather Summary: </span>" + object.currently.summary
+		+ "<br/><br/> <span class='summaryTitles'>Forecast Summary: </span>" + object.hourly.summary;
 	document.getElementById("current-icon").style.backgroundColor = "hsl(216, 100%, 60%)";
 	document.getElementById("weather-summary").style.backgroundColor = "hsl(216, 100%, 60%)";
-	alerts.innerHTML = "Alerts: " + object.alerts.title;
-	summary.innerHTML = object.hourly.summary;
-
-	console.log(object);
+	forecastValidTime = new Date(object.currently.time * 1000);
+	if (object.currently.uvIndex > 5) {
+		document.getElementById("current-uvIndex").style.color = "red";
+	} else {
+		if (object.currently.uvIndex < 6 && object.currently.uvIndex > 3 ) {
+			document.getElementById("current-uvIndex").style.color = "dark-green";
+		}
+	}
+	alertsTitle = object.alerts.title;
+	console.log(object.currently.cloudCover + "Cloud Cover");
+	console.log("Storm: " + object.currently.nearestStormDistance);
+	console.log("Wind Gust: " + object.daily.windGustTime);
+	console.log(forecastValidTime);
+	//console.log("alerts: " + object.alerts);
+	document.getElementById("alerts").style.display = 'block';
+	if (alertsTitle) {
+		alertsTitle = (object.alert.summary);
+	} else {
+		alerts.innerHTML = "No Alerts as of " + timeConvert(forecastValidTime);
+	}
+	summary = document.getElementById("summary");
+	document.getElementById("alerts").style.visibility = "visible";
 }
+
+let timeConvert = function(d) {
+	return datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+		d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+};
